ef02e417e5422f46873fb0fca2d36338d5dd914c
DDR2AS <diego.avila@pucp.edu.pe>
2022-08-11 10:48:10 -0500

    Se agregaron las contraseñas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from secret import password
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     print('jaguar ')
     print('segunda linea')
     print('test3')
+    print(password)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
