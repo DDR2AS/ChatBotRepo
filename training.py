8391fc683ab2cad21a7b2cc9413a4b3d6c50441b
tryhard <haroldlinorm@gmail.com>
2022-08-11 15:50:09 -0500

    añadiendo un chiqui gaa

--- training.py
+++ training.py
@@ -3,16 +3,17 @@ import json
 import pickle
 import nltk
 import numpy as np
-from nltk.stem import WordNetLemmatizer
+from nltk import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizercd
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,Activation,Dropout
-from tensorflow.keras.optimizers import SGD
+# from tensorflow.keras.layers import Dense,Activation,Dropout
+# from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json').read()) #Creación de diccionario
+intents = json.loads(open('intents.json').read()) # Creación de diccionario
 
-#Creamos 3 listas vacias
+# Creamos 3 listas vacias
 words = []
 classes = []
 documents = []
@@ -55,6 +56,4 @@ training = np.array(training)
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 
-print(training)
-
-print("gaa")
+model = Sequential()
