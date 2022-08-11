0b777a2a9fefb955b12d1b10af5a694d3a0efa2f
DDR2AS <diego.avila@pucp.edu.pe>
2022-08-11 10:40:58 -0500

    Added more print lines

--- main.py
+++ main.py
@@ -13,5 +13,8 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Diego Alonso Avila')
     print('jaguar ')
+    print('segunda linea')
+    print('test3')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
