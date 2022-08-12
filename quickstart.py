f99da45f4c02f2533e08ee5aed4eec78395b4793
DDR2AS <diego.avila@pucp.edu.pe>
2022-08-12 14:14:15 -0500

    Obteniendo eventos y creando nuevo evento con errores

--- quickstart.py
+++ quickstart.py
@@ -22,7 +22,54 @@ result = service.calendarList().list().execute()
 
 
 print(result['items'][2])
+#Imprimimos todas las keys del diccionario de resultados
+print(result['items'][2].keys())
 
-for i in range(0,len(result['items'])):
-    print(result['items'][i]['summary'])
+#for i in range(0,len(result['items'])):
+#    print(result['items'][i])
 
+#Obtener eventos del calendario
+calendar_id = result['items'][2]['id']
+print(calendar_id)
+result = service.events().list(calendarId=calendar_id,timeZone = "America/Bogota").execute()
+print('\n')
+print(result)
+
+
+#Creación de evento
+from datetime import datetime, timedelta
+start_time = datetime(2022, 8, 12, 19, 30, 0)
+end_time = start_time + timedelta(hours=2)
+
+print(f'Start time {start_time}')
+print(f'time delta = {timedelta(hours=4)}\n End time  = {end_time}')
+
+#Body of the event
+def CreateBodyEvent(summary,location,description,startTime,endTime,TimeZone):
+    eventBody = {
+      'summary': summary,
+      'location': location,
+      'description': description,
+      'start': {
+        'dateTime': startTime.strftime("%Y-%m-%dT%H:%M:%S"),
+        'timeZone': TimeZone,
+      },
+      'end': {
+        'dateTime': endTime.strftime("%Y-%m-%dT%H:%M:%S"),
+        'timeZone': TimeZone,
+      },
+      'reminders': {
+        'useDefault': False,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+    return eventBody
+
+timezone = "America/Bogota"
+
+#Usando nuestra función
+event = CreateBodyEvent('Cita con caardiologo','Hosp. Almenara','Consulta general',start_time,end_time,timezone)
+service.events().insert(calendarId=calendar_id, body=event).execute()
