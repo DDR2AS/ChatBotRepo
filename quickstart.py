5df6316875b3defb7a137cd69542ee500a37d539
DDR2AS <diego.avila@pucp.edu.pe>
2022-08-12 09:04:48 -0500

    Corrección de errores en setup de clientee

--- quickstart.py
+++ quickstart.py
@@ -1,12 +1,11 @@
 import pickle
-import apiclient
 
-from apiclient.discovery import build
+from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 ##SETUP OAUTH 2.0 SETUP
 #We have to specify some scopes https://developers.google.com/calendar/api/guides/auth
-scopes = ['https://www.googleapis.com/auth/calendar.events']
+scopes = ['https://www.googleapis.com/auth/calendar']
 
 #Then we have to create a flow
 flow = InstalledAppFlow.from_client_secrets_file("client_secret.json",scopes=scopes)
@@ -14,7 +13,16 @@ credentials = flow.run_console()
 
 #Then we have to create credential
 pickle.dump(credentials,open("token.pkl","wb")) #write binary stream
-credentials = pickle.load("token.pkl", "rb") #read
+credentials = pickle.load(open("token.pkl", "rb")) #read
 print(credentials)
 
-service =build("calendar")
+service = build("calendar","v3",credentials=credentials)
+
+result = service.calendarList().list().execute()
+
+
+print(result['items'][2])
+
+for i in range(0,len(result['items'])):
+    print(result['items'][i]['summary'])
+
